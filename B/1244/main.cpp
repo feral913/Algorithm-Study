30727061aacf96dc0c2b022afa97d2dc7b108c07
snrndi121 <snrndi4521@gmail.com>
2018-12-26 20:08:57 +0900

    tq : testcase) '32888 2'만 안됨

--- B/1244/main.cpp
+++ B/1244/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <algorithm>
 using namespace std;
 
 //def
@@ -58,117 +58,97 @@ void file_output(vvtype _output)
 		}
 		return ;
 }
-void swap(char* a, char* b) {
+void swap(char* a, char* b)
+{
 		char temp = *a;
 		*b = *a;
 		*a = temp;
 }
-/*
+bool IsDuplicated(vtype& src)
+{
+		int base = 0;
+		for (uint next = base + 1; next < src.size(); ++next) {
+				if (src[ base ] == src[ next ] ) {
+						return true;
+				}
+				base = next;
+		}
+		return false;
+}
 vtype solve_algorithm(Input _rule)
 {
+		const uint MAX_CUR = _rule.num.size() - 2;
 		uint cur = 0;
 		while(_rule.count > 0) {
 				cout << " #### cycle start #### " << endl;
 				cout << " > count : " << _rule.count << endl;
+				cout << " > cur : " << cur << endl;
 				for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
 				//
-				// 제일 큰 자리수 찾기
+				//1. 숫자열에서 가장 큰 값이 있는 곳의 인덱스를 찾아냄
 				uint max = cur;
 				for (uint i = cur + 1; //current 이전의 값들을 무시
 						i < _rule.num.size(); ++i) {
-						cout << "(" << i << ")cur : " << _rule.num[i];
+						//가장 뒤에 있는 가장 큰 값을 max로 찾게됨
 						if (_rule.num[max] <= _rule.num[i]) {
 								max = i;
-								cout << ", max : " << _rule.num[max] << endl;
-						} else { cout << endl;}
+						}
 				}
-				// 현재 current와 max 스위치
+				//431159 7
+				//931154 6
+				//951134 5
+				//954131 4
+				//954311 3
+				//954311 2
+				// 2. 정렬
 				if (max != cur) {
-						cout << " >> find max " << endl;
-						cout << " > cur : " << _rule.num[cur] << endl;
-						cout << " > max : " << _rule.num[max] << endl;
-						if (cur < _rule.num.size() - 2) {
-								swap(_rule.num[cur], _rule.num[max]);
-								cur++; _rule.count--;
-						} else {
-								cout << " >> no max " << endl;
-						}
-						for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
-						cout << " #### cycle end #### " << endl;
-				}
-				else if (cur == _rule.num.size() - 2){
-						//마지막 꼬리 바꾸기
-						uint sz = _rule.num.size();
-						while(_rule.count-- > 0) {
-								cout << " # tail start# " << endl;
-								for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
-								//
-								swap(_rule.num[sz - 2], _rule.num[sz - 1]);
-								//
-								for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
-								cout << " # tail end #### " << endl;
-						}
-				} else {
+						//< 정렬 시도 >
+						cout << " >> try sort " << endl;
+						swap(_rule.num[cur], _rule.num[max]);
 						cur++;
-						cout << " > nothing hapeens" << endl;
-						cout << " #### cycle end #### " << endl;
+						_rule.count--;
 				}
-		}
-
-		return _rule.num;
-}
-*/
-vtype solve_algorithm(Input _rule)
-{
-		uint cur = 0;
-		while(_rule.count > 0) {
-				cout << " #### cycle start #### " << endl;
-				cout << " > count : " << _rule.count << endl;
-				for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
+				//cur이 최대 비교 도달 지점 전이라면
+				else if (cur < MAX_CUR ){
+						cout << " >> cur is not end " << endl;
+						//만약 cur이 끝가지 도달 안했으면 더 해보는게 맞음
+						//< 정렬 완료 -> 끝까지 확인 시도 >
+						//78466 6
+						cur++;
+				}
+				//32888 2
 				//
-				// 제일 큰 자리수 찾기
-				uint max = -1;//0;//cur;
-				for (uint i = 0;//cur + 1; //current 이전의 값들을 무시
-						i < _rule.num.size(); ++i) {
-						cout << "(" << i << ")cur : " << _rule.num[i];
-						if (_rule.num[max] <= _rule.num[i]) {
-								max = i;
-								cout << ", max : " << _rule.num[max] << endl;
-						} else { cout << endl;}
-				}
-				// 현재 current와 max 스위치
-				if (max != -1) {//cur) {
-						cout << " >> find max " << endl;
-						//cout << " > cur : " << _rule.num[cur] << endl;
-						cout << " > max : " << _rule.num[max] << endl;
-						if (cur < _rule.num.size() - 2) {
-								swap(_rule.num[cur], _rule.num[max]);
-								//cur++; 
-								_rule.count--;
+				//cur이 최대 비교 지점까지 도달하면
+				else {
+						//만약 cur이 끝까지 도달을 했으면 다시 cur을 처음으로 돌려서 한번더 확인
+						// < 정렬 완료 + 끝까지 확인 완료 >
+						//중복지점 찾기
+						cout << " >> sort done + cur done" << endl;
+						bool dup_point = IsDuplicated(_rule.num);
+						if (dup_point != false) {
+								cout << " >> dup is in a raw" << endl;
+								//정렬된 순열내 중복이 존재하면 추가 정렬 종료
+								//ex) 정렬결과 : 54331, 남은 카운트 : 5
+								//3, 3끼리 교환이 계속 될꺼니깐 추가 정렬 의미가 없음
+								_rule.count = 0;
 						} else {
-								cout << " >> no max " << endl;
+								cout << " >> no dup is in a raw" << endl;
+								//정렬된 순열내 중복이 없다면
+								//ex) 정렬결과 : 54321, 남은 카운트 : 5
+								//최대를 만들기 위하여 맨 끝자리와 그 이전값이 반복적으로 교환될 것이므로
+								if (_rule.count % 2 == 0) {
+										cout << " >> no action done" << endl;
+								} //짝수번이면 제자리
+								else {
+									cout << " >> one swap done " << endl;
+									int tail = _rule.num.size() - 1;
+									swap(_rule.num[ tail - 1], _rule.num[ tail ]);
+								} //홀수번이면 한번만 교환
+								_rule.count = 0;
 						}
-						for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
-						cout << " #### cycle end #### " << endl;
 				}
-				else if (cur == _rule.num.size() - 2){
-						//마지막 꼬리 바꾸기
-						uint sz = _rule.num.size();
-						while(_rule.count-- > 0) {
-								cout << " # tail start# " << endl;
-								for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
-								//
-								swap(_rule.num[sz - 2], _rule.num[sz - 1]);
-								//
-								for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
-								cout << " # tail end #### " << endl;
-						}
-				} else {
-						cur++;
-						cout << " > nothing hapeens" << endl;
-						cout << " #### cycle end #### " << endl;
-				}
+				for (vtype::iterator it = _rule.num.begin(); it != _rule.num.end(); ++it) { cout << *it;}cout << endl;
+				cout << " #### cycle end #### " << endl;
 		}
-
 		return _rule.num;
 }
