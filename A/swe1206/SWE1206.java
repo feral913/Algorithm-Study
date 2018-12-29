5aec65cff57e45e96d8ca3b9038a240d2a28a573
snrndi121 <snrndi4521@gmail.com>
2018-12-29 23:28:31 +0900

    -

---
+++ A/swe1206/SWE1206.java
@@ -0,0 +1,69 @@
+/*
+ * x축은 건물이 놓인 칸, y축은 건물의 높이(층수)이다.
+ * 기준 건물의 왼쪽, 오른쪽 각 두칸 내에 있는 건물에 가리지 않는 만큼 해당 건물은 조망권이 확보된다.
+ * 테스트 케이스 내 전체 건물에서 조망권이 확보된 총 층의 수를 구하라.
+ */
+
+package swe1206;
+
+import java.util.Scanner;
+
+// 건물 별 높이 정보를 갖는 Building 객체.
+class Building{
+	private int[] height;
+	
+	public Building(int maxWidth) {
+		this.height = new int[maxWidth];
+	}
+	
+	public int getHeight(int i) {
+		return height[i];
+	}
+	
+	public void setHeight(int i, int height) {
+		this.height[i] = height;
+	}
+}
+
+public class SWE1206 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		for(int testCase = 1; testCase <= 10; testCase++) {
+			int maxWidth = sc.nextInt();
+			int result = 0;
+			
+			Building bd = new Building(maxWidth);
+			
+			for(int i = 0; i < maxWidth; i++) {
+				bd.setHeight(i, sc.nextInt());
+			}
+
+			// 가장 왼쪽과 오른쪽 각각 두칸(0, 1, maxWidth - 1, maxWidth - 2)에는 건물이 없다.
+			// 기준 건물에 대해서 좌우 두칸 내에 있는 건물들 중 가장 높은 건물의 높이를 구한다.
+			// 만약 기준 건물이 좌우 두칸 내에 있는 건물 중 가장 높은 건물보다 높다면, 기준 건물에서 가장 높은 건물의 높이를 뺀 만큼 기준 건물은 조망권이 확보된 층을 갖고 있다.
+			for(int i = 2; i < maxWidth - 2; i++) {
+				int lTopBuilding = 0;
+				int rTopBuilding = 0;
+				int topBuilding = 0;
+				
+				if(bd.getHeight(i - 2) > bd.getHeight(i - 1)) lTopBuilding = i - 2;
+				else lTopBuilding = i - 1;
+				
+				if(bd.getHeight(i + 2) > bd.getHeight(i + 1)) rTopBuilding = i + 2;
+				else rTopBuilding = i + 1;
+				
+				if(bd.getHeight(lTopBuilding) > bd.getHeight(rTopBuilding)) topBuilding = lTopBuilding;
+				else topBuilding = rTopBuilding;
+
+				if(bd.getHeight(i) > bd.getHeight(topBuilding)) {
+					result += bd.getHeight(i) - bd.getHeight(topBuilding);
+				}
+			}
+			
+			System.out.println("#" + testCase + " " + result);
+		}
+	}
+
+}
---
+++ A/swe1244/SWE1244.java
@@ -0,0 +1,97 @@
+package swe1244;
+
+import java.util.Scanner;
+
+class NumBoard{
+	private int numLength;
+	private int[] numSrc;
+	private int number;
+	private int largestNum;
+	
+	public NumBoard(int numLength, int tradeChance) {
+		this.numLength = numLength;
+		numSrc = new int[numLength];
+	}
+	
+	public int[] getNumSrc() {
+		return numSrc;
+	}
+	
+	public void setNumSrc(int i, int num) {
+		numSrc[i] = num;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public int getLargestNum() {
+		return largestNum;
+	}
+	
+	public void setLargestNum(int num) {
+		if(largestNum < num) largestNum = num;
+	}
+	
+	public void tradeBoard(int i, int j) {
+		int temp = numSrc[i];
+		numSrc[i] = numSrc[j];
+		numSrc[j] = temp;
+	}
+	
+	public void toInteger() {
+		number = 0;
+		
+		for(int i = 0; i < numLength; i++) {
+			number += numSrc[numLength - i - 1] * Math.pow(10, i);
+		}
+	}
+}
+
+public class SWE1244 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int testCase = 1; testCase <= T; testCase++) {
+			String str = sc.next();
+			int tradeChance = sc.nextInt();
+			
+			NumBoard nb = new NumBoard(str.length(), tradeChance);
+			
+			for(int i = 0; i < str.length(); i++) {
+				nb.setNumSrc(i, str.charAt(i) - '0');
+			}
+
+			trade(str.length(), tradeChance, nb);
+
+			System.out.println("#" + testCase + " " + nb.getLargestNum());
+		}
+
+	}
+	
+	public static void trade(int numLength, int tradeChance, NumBoard nb) {
+		for(int i = 0; i < numLength; i++) {
+			for(int j = i + 1; j < numLength; j++) {
+				if(nb.getNumSrc()[i] == nb.getNumSrc()[j]) continue;
+				
+				if(tradeChance > 1) {
+					nb.tradeBoard(i, j);
+					nb.toInteger();
+					tradeChance--;
+					trade(numLength, tradeChance, nb);
+					tradeChance++;
+				} else {
+					nb.tradeBoard(i, j);
+					nb.toInteger();
+					nb.setLargestNum(nb.getNumber());
+					nb.tradeBoard(i, j);
+					nb.toInteger();
+				}
+			}
+		}
+	}
+
+}
