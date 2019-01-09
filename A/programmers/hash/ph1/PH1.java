5b417d561e7d2dbf5e606015aa178c4ec3ffaf55
Falunhua <Falunhua@DESKTOP-AOFA4T4>
2019-01-09 13:40:00 +0900

    fin ph1

--- A/programmers/hash/ph1/PH1.java
+++ A/programmers/hash/ph1/PH1.java
@@ -76,67 +76,37 @@ public class PH1 {
 	// 중복되는 이름이 있기 때문에 한번 찾은 이름은 완주자 목록에서 삭제한다.
 	// 그렇게 하니까 시간초과 나왔다.
 	// 아 이름을 key에다가 넣어야되나....?? 아 그러면 애초에 동일 이름을 중복 저장못하는구나...
-	
+	// 해결 : 이중 loop문이 이중 이상 생기지 않도록 고민해보았다.
+	// 완주자를 Hash map에 넣어준다. 이때 Hash map의 key 값으로 이름을 넣고 value를 0에서부터 이름이 중복되는 만큼 +1 해주었다.
+	// 참가자의 수만큼 loop를 돌면서 완주자의 Hash map에 참석자의 이름이 있는지 확인한다.
+	// 이때 참석자와 같은 이름이 한명이상 완주자 hash에 있다면 value 값을 -1 해준다.
+	// 참석자의 이름이 완주자 hash에 value 0으로 있다면 완주자 hash에서 해당 이름의 key 값을 삭제한다.
+	// 참석자의 이름이 완주자 hash에 없으면 탐색을 종료하고 해당 이름을 반환한다.
 	public static String solution(String[] participant, String[] completion) {
 		String answer = "";
 
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
 		
 		for(int i = 0; i < completion.length; i++) {
-			hm.put(completion[i], i);
-		}
-		
-		for(int i = 0; i < participant.length; i++) {
-			if(hm.containsKey(participant[i])) {
-				hm.remove(participant[i]);
+			if(hm.containsKey(completion[i])) {
+				hm.put(completion[i], hm.get(completion[i]) + 1);
 			} else {
-				answer = participant[i];
+				hm.put(completion[i], 0);
 			}
 		}
 		
-/*		HashMap<Integer, String> parHM = new HashMap<Integer, String>();
-		HashMap<Integer, String> comHM = new HashMap<Integer, String>();
-		
-		int key = 0;
-		
-		for(int i = 0; i < participant.length; i++) {
-			//hm.put(key, completion[i]);
-			parHM.put(key, participant[i]);
-			if(i < (participant.length - 1)) {
-				comHM.put(key, completion[i]);
-			}
-			key++;
-		}
-		
-
-		for(int i = 0; i < completion.length; i++) {
-			if(comHM.containsKey(participant[i])) {
-				
-			}
-		}*/
-		
-		//HashMap<Integer, String> hm = new HashMap<Integer, String>();
-/*		for(Object o : comHM.keySet()) {
-			for(int i = 0; i < )
-			if(comHM.containsValue(parHM.get(o))) {
-				comHM.remove(o);
+		for(int i = 0; i< participant.length; i++) {
+			String name = participant[i];
+			
+			if(hm.containsKey(name) && hm.get(name) > 0) {
+				hm.put(name, hm.get(name) - 1);
+			} else if(hm.containsKey(name) && hm.get(name) == 0) {
+				hm.remove(name);
 			} else {
-				answer = parHM.get(o);
+				answer = name;
+				break;
 			}
-		}*/
-		
-/*		for(int i = 0; i < participant.length; i++) {
-			if(hm.containsValue(participant[i])) {
-				for(Object o : hm.keySet()) {
-					if(hm.get(o).equals(participant[i])) {
-						hm.remove(o);
-						break;
-					}
-				}
-			} else {
-				answer = participant[i];
-			}
-		}*/
+		}		
 		
 		return answer;
 	}
