667499684713af739f08c2023d3d57d447044900
Falunhua <Falunhua@DESKTOP-AOFA4T4>
2019-01-10 12:17:31 +0900

    start ph2

---
+++ A/programmers/hash/ph2/PH2.java
@@ -0,0 +1,24 @@
+package programmers.hash.ph2;
+
+import java.util.Scanner;
+
+class Solution{
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+		
+		return answer;
+	}
+}
+
+public class PH2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int testCase = 0; testCase < T; testCase++) {
+
+		}
+	}
+
+}
