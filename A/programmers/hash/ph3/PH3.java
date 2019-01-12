3cc0b5ac74aa9c2bbbcab90fb8a1e2a51e004b4b
Falunhua <Falunhua@DESKTOP-AOFA4T4>
2019-01-12 11:51:35 +0900

    start ph3

---
+++ A/programmers/hash/ph3/PH3.java
@@ -0,0 +1,16 @@
+package programmers.hash.ph3;
+
+class Solution{
+	public int solution(String[][] clothes) {
+		int answer = 0;
+		return answer;
+	}
+}
+
+public class PH3 {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+	}
+
+}
