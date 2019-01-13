4fc1dcd63238d4a1cd7c8de5e8e752681ecf7a14
Feral <Feral@DESKTOP-HB33UVJ>
2019-01-13 15:06:58 +0900

    fin ph3

--- A/programmers/hash/ph3/PH3.java
+++ A/programmers/hash/ph3/PH3.java
@@ -47,9 +47,15 @@ class Clothes{
 // 1, 2, 3, 4 = 1 + 2 + 3 + 4 + 1 * 2 + 1 * 3 + 1 * 4 + 2 * 3 + 2 * 4 + 3 * 4 + 1 * 2 * 3 + ...
 // 그렇게 곱하려면 결국 조합을 만들어야겠다..
 // 됐다. 근데 다 되는데 한가지 테스트 케이스에서 시간초과가 난다. 30가지 모두 다른 의상의 종류로 돌려보니 시간이 엄청 오래 걸린다. 어떻게 줄일 수 있을까.
+// 와씨 충격적이다. 시간초과에 대한 누군가의 질문에 가능한 모든 조합의 수를 구하는 방법을 올려놨다.
+// 특정 옷의 종류에 대해서 옷의 수가 있을 때 해당 종류의 옷을 입지 않는 경우까지 고려해서 옷의 수에 1을 더해준다.
+// 그렇게 구해진 각 종류별 옷의 수를 다 곱하면 모든 종류의 옷을 안입는 경우부터 하나만 입는 경우랑 쭉쭉쭉해서 모든 종류의 옷을 다 입는 경우의 수까지 다 합한 값이 나온다.
+// 거기서 아무것도 안입는 경우는 없으니 1을 빼면....
+// ex) 머리 : 3, 얼굴 : 2, 몸 1 => (3 + 1) * (2 + 1) * (1 + 1) - 1 = 13.
+// 헛수고한 거 같지만 그래도 조합공부했으니..
 class Solution{
 	public int solution(String[][] clothes) {
-		int answer = 0;
+		int answer = 1;
 		
 		// HashMap Key에 의상 종류, Value에 의상 수를 저장한다.
 		HashMap<String, Integer> hm = new HashMap<>();
@@ -58,8 +64,13 @@ class Solution{
 				hm.put(clothes[i][1], 1);
 			else hm.put(clothes[i][1], hm.get(clothes[i][1]) + 1);
 		}
+
+		for(Object o : hm.keySet()) {
+			answer *= hm.get(o) + 1;
+		}
+		answer--;
 		
-		// 의상 종류를 ArrayList에 따로 저장한다. 
+/*		// 의상 종류를 ArrayList에 따로 저장한다. 
 		ArrayList<Object> arr = new ArrayList<>();
 		for(Object o : hm.keySet()) {
 			arr.add(o);
@@ -69,12 +80,12 @@ class Solution{
 		int[] intArr = new int[hm.size()];
 		for(int i = 1; i <= hm.size(); i++) {
 			answer = comb(intArr, 0, i, hm, arr, 0, answer);
-		}
+		}*/
 		
 		return answer;
 	}
 	
-	public int comb(int[] intArr, int index, int r, HashMap<String, Integer> hm, ArrayList<Object> arr, int target, int answer) {
+/*	public int comb(int[] intArr, int index, int r, HashMap<String, Integer> hm, ArrayList<Object> arr, int target, int answer) {
 		if(r == 0) {
 			int temp = 1;
 			for(int i = 0; i < index; i++) {
@@ -91,7 +102,7 @@ class Solution{
 		}
 		
 		return answer;
-	}
+	}*/
 }
 
 public class PH3 {
