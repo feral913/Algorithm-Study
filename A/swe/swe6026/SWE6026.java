8594f0d6150a4dc722c9d8b3a018da7c20ab4f13
Falunhua <Falunhua@DESKTOP-AOFA4T4>
2019-01-18 14:34:49 +0900

    hold swe6026

--- A/swe/swe6026/SWE6026.java
+++ A/swe/swe6026/SWE6026.java
@@ -7,22 +7,23 @@
 
 package swe.swe6026;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 class Password{
-	private long keyNum;
-	private long length;
+	private int keyNum;
+	private int length;
 	
-	public Password(long M, long N) {
+	public Password(int M, int N) {
 		keyNum = M;
 		length = N;
 	}
 	
-	public long getKeyNum() {
+	public int getKeyNum() {
 		return keyNum;
 	}
 	
-	public long getLength() {
+	public int getLength() {
 		return length;
 	}
 }
@@ -34,28 +35,52 @@ public class SWE6026 {
 		int T = sc.nextInt();
 		
 		for(int testCase = 1; testCase <= T; testCase++) {
-			long M = sc.nextInt();
-			long N = sc.nextInt();
+			int M = sc.nextInt();
+			int N = sc.nextInt();
 			
 			Password pass = new Password(M, N);
+			HashMap<Integer, Integer> check = new HashMap<>();
+			for(int i = 0; i < M; i++) {
+				check.put(i, 0);
+			}
 			
-			System.out.println("#" + testCase + " " + solution(pass.getKeyNum(), pass.getLength()));
+			System.out.println("#" + testCase + " " + solution(pass.getKeyNum(), pass.getLength(), check, 0));
 		}
 	}
 	
-	// M 개의 문자를 모두 이용해서 만들 수 있는 길이 N의 문자열의 수를 구한다.
-	// M 개의 문자를 이용해서 만들 수 있는 길이 N의 문자열의 수에서 M 개의 문자 중 하나 이상 사용하지 않고 만들 수 있는 길이 N의 문자열의 수를 뺸다.
-	// M 개의 문자 중 하나만 이용해서 만들 수 있는 길이 N의 문자열 = M 개.
-	// M 개의 문자 중 두개만 이용해서 만들 수 있는 길이 N의 문자열 = C(M, 2) * 2^N.
-	public static long solution(long M, long N) {
+	// M 개의 문자가 있다.
+	// 길이 N의 문자열을 만든다.
+	// -> 만들 수 있는 모든 경우의 수는 M^N.
+	// 이때 M 개의 문자 중 하나의 문자라도 사용되지 않은 경우를 제외한다.
+	// 그렇다면 M 개의 문자 중 하나의 문자를 뽑는 작업을 N 만큼 수행한다.
+	// 그리고 마지막에 M 개의 문자 중 하나의 문자라도 사용되지 않은 문자가 있으면 해당 문자열은 제외한다.
+	// 자리 수가 길어질 수록 시간이 너무 오래 걸린다.
+	// 버퍼를 사용하거나 수식으로 해결하는 등 다른 방법을 찾아야겠다.
+	public static long solution(int M, int N, HashMap<Integer, Integer> check, long answer) {
 		final int cut = 1000000007;
 		
-		long answer = (long)(Math.pow(M, N));
-		for(int i = 1; i < M; i++) {
-			answer = answer - (long)(Math.pow(M - i, N) * combination(M, M - i)); 
+		// 문자열이 끝났으면.
+		if(N == 0) {
+			// M 개의 문자 중 사용되지 않은 문자가 없으면 카운트 + 1.
+			if(!check.containsValue(0)) answer++;
+			return answer;
 		}
 		
+		// M 개의 문자를 순서대로 하나 씩 뽑는다.
+		// 하나를 뽑고 다음 문자를 뽑는다.
+		for(int i = 0; i < M; i++) {
+			check.put(i, check.get(i) + 1);
+			answer = solution(M, N - 1, check, answer);
+			check.put(i, check.get(i) - 1);
+		}
+		
+		answer %= cut;
+				
 		return answer;
+	}
+	
+	public static long solution2(int M, int N, int i) {
+		return 0;
 	}
 	
 	public static long factorial(long number, long ret) {
